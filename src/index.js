e3fe0d555c53dab25b2e615013a87c52f69a71d4
velopert <public.velopert@gmail.com>
2017-01-15 12:57:10 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
+
+import App from './App';
+import Home from './containers/Home';
+import About from './containers/About';
+import Posts from './containers/Posts';
+import Post from './containers/Post';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import './index.css';
 
 ReactDOM.render(
-  <Routes/>,
+    <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="about" component={About}/>
+      <Route path="post" component={Posts}>
+        <Route path=":id" component={Post}/>
+      </Route>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
